7d23a2f77508d3fb8cbfd808a0dcc8c96bd7c9de
LincAnderson <36419018+LincAnderson@users.noreply.github.com>
2020-02-07 11:56:53 -0500

    Initial Commit

---
+++ Text Image Classifier(ASM)Tests/Text_Image_Classifier_ASM_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Text_Image_Classifier_ASM_Tests.swift
+//  Text Image Classifier(ASM)Tests
+//
+//  Created by lincoln anderson on 2/7/20.
+//  Copyright © 2020 lincoln anderson. All rights reserved.
+//
+
+import XCTest
+@testable import Text_Image_Classifier_ASM_
+
+class Text_Image_Classifier_ASM_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
